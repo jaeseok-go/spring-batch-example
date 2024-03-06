7946e0f81a3244baca695f019c03b3397dd001ce
jeeyulee <jeeyu-lee@naver.com>
2024-03-06 23:50:42 +0900

    feat: JobParameter 접근

--- src/main/java/com/example/springbatchexample/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/springbatchexample/job/SimpleJobConfiguration.java
@@ -2,10 +2,13 @@ package com.example.springbatchexample.job;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -22,12 +25,17 @@ public class SimpleJobConfiguration {
     @Bean
     public Step step(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
         return new StepBuilder("step1", jobRepository)
-                .tasklet(
-                        (contribution, chunkContext) -> {
-                            System.out.println("Hello, World!");
-                            return RepeatStatus.FINISHED;
-                        },
+                .tasklet(helloWorldTasklet(null),
                         platformTransactionManager)
                 .build();
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet helloWorldTasklet(@Value("#{jobParameters['name']}") String name) {
+        return (contribution, chunkContext) -> {
+            System.out.println(String.format("Hello, %s!", name));
+            return RepeatStatus.FINISHED;
+        };
     }
 }
